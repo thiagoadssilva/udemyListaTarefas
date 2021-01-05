dc6de217904b15712eb32c68d1fdd92437586429
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-05 20:58:13 -0300

    Iniciando o carregamento do componente que vai listar as tarefas

--- frontend/src/components/ListaTarefas/index.js
+++ frontend/src/components/ListaTarefas/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 import {
@@ -7,11 +10,45 @@ import {
 } from './styled';
 
 export default () => {
-    return(
+    const [tarefas, setTarefas] = useState([]);
+    const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+    useEffect(() => {
+        function obterTarefas(){
+            const tarefasDb = localStorage['tarefas'];
+            let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTarefas(listarTarefas);
+        }
+
+        if(carregarTarefas){
+            obterTarefas();
+            setCarregarTarefas(false);
+        }
+        
+    }, [carregarTarefas]);
+
+    return (
         <Container>
-             <A href="/cadastrar" className="btn btn-success bnt-sm" >
-                Nova Tarefa
-            </A>
+            <div className="text-center">
+                <h3>Lista a Fazer</h3>
+                <Table striped bordered hover responsive data-testid="tabela">
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>
+                                <A href="/cadastrar" className="btn btn-success btn-sm" data-testid="btn-nova-tarefa">
+                                    <FontAwesomeIcon icon={faPlus} />
+                                    &nbsp;
+                                    Nova Tarefa
+                                </A>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </Table>
+            </div>
         </Container>
     );
 }
