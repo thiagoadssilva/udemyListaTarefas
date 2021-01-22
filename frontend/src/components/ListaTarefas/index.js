d6138c95c3a078603ab4a7cb837474e316098b89
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-21 21:05:04 -0300

    Colocando a funcionalidade de listar, ordenar, filtrar e paginaçao usando api

--- frontend/src/components/ListaTarefas/index.js
+++ frontend/src/components/ListaTarefas/index.js
@@ -3,17 +3,22 @@ import { A } from 'hookrouter';
 import { Table, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios';
 
 import ItensListaTarefas from '../ItensListaTarefas';
 import Paginacao from '../Paginacao';
 import Ordenacao from '../Ordenacao';
+
+
 
 import {
     Container
 } from './styled';
 
 export default () => {
+
     const ITEMS_POR_PAG = 5;
+    const API_URL_LISTAR_TAREFAS = "http://localhost:3001/gerenciador-tarefas";
 
     const [tarefas, setTarefas] = useState([]);
     const [carregarTarefas, setCarregarTarefas] = useState(true);
@@ -24,21 +29,41 @@ export default () => {
     const [filtroTarefa, setFiltroTarefa] = useState('');
 
     useEffect(() => {
-        function obterTarefas() {
-            const tarefasDb = localStorage['tarefas'];
-            let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-            //- Filtrar a tarefa
-            listaTarefas = listaTarefas.filter(t => t.nome.toLowerCase().indexOf(filtroTarefa.toLocaleLowerCase()) === 0);
-            //- Ordenação
-            if (ordenarAsc) {
-                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
-            } else if (ordenarDesc) {
-                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
-            }
-            //- Paginação
-            setTotalItems(listaTarefas.length);
-            setTarefas(listaTarefas.splice((paginaAtual - 1) * ITEMS_POR_PAG, ITEMS_POR_PAG));
+        async function obterTarefas() {
 
+            //- INICIO Criando a ordenação
+            let ordem = '';
+
+            if (ordenarAsc) {
+                ordem = 'ASC';
+            } else if (ordenarDesc) {
+                ordem = 'DESC';
+            }
+            //- FIM Criando a ordenação
+
+            try {
+                const params = `?pag=${paginaAtual}&ordem=${ordem}&filtro-tarefa=${filtroTarefa}`;
+                let { data } = await axios.get(API_URL_LISTAR_TAREFAS + params);
+                setTotalItems(data.totalItens);
+                setTarefas(data.tarefas);
+            } catch (error) {
+                setTarefas([]);
+            }
+            //- INCIO Código que representa a criação do localstorage, foi comentado para entrada da api
+            // const tarefasDb = localStorage['tarefas'];
+            // let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            // //- Filtrar a tarefa
+            // listaTarefas = listaTarefas.filter(t => t.nome.toLowerCase().indexOf(filtroTarefa.toLocaleLowerCase()) === 0);
+            // //- Ordenação
+            // if (ordenarAsc) {
+            //     listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+            // } else if (ordenarDesc) {
+            //     listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
+            // }
+            // //- Paginação
+            // setTotalItems(listaTarefas.length);
+            // setTarefas(listaTarefas.splice((paginaAtual - 1) * ITEMS_POR_PAG, ITEMS_POR_PAG));
+            //- FIM Código que representa a criação do localstorage, foi comentado para entrada da api
         }
 
         if (carregarTarefas) {
@@ -69,7 +94,7 @@ export default () => {
         setCarregarTarefas(true);
     }
 
-    function handleFiltrar(event){
+    function handleFiltrar(event) {
         setFiltroTarefa(event.target.value);
         setCarregarTarefas(true);
     }
@@ -106,7 +131,7 @@ export default () => {
                             <th>
                                 <Form.Control placeholder="Informa uma tarefa" className="filtroTarefa" type="text" value={filtroTarefa} onChange={handleFiltrar} data-testid="txt-tarefa" />
                             </th>
-                            <th>&nbsp;</th>    
+                            <th>&nbsp;</th>
                         </tr>
                     </thead>
                     <tbody>
