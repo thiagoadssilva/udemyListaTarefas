55269341397bbaab2c930a8b7735d665bb94d2df
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 16:21:52 -0300

    Implementaçao dos teste no compoenente cadastrar

--- frontend/src/components/CadastrarTarefa/cadastrarTarefa.test.js
+++ frontend/src/components/CadastrarTarefa/cadastrarTarefa.test.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CadastrarTarefa from './index';
+import axiosMock from 'axios';
 
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-describe.skip('Teste do componente Cadastrar Tarefa', () => {
-    it('Deve redenrizar sem erros', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<CadastrarTarefa />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
+describe('Teste do componente Cadastrar Tarefa', () => {
+    // it('Deve redenrizar sem erros', () => {
+    //     const div = document.createElement('div');
+    //     ReactDOM.render(<CadastrarTarefa />, div);
+    //     ReactDOM.unmountComponentAtNode(div);
+    // });
 
-    it('Deve cadastrar uma tarefa', () => {
-        const { getByTestId} = render(<CadastrarTarefa />);
+    it('Deve cadastrar uma tarefa', async () => {
+        const { getByTestId, findByTestId} = render(<CadastrarTarefa />);
 
         fireEvent.change(getByTestId('txt-tarefa'), { target: { value: 'Testar Componente' } });
         fireEvent.click(getByTestId('btn-cadastrar'));
-        expect(getByTestId('modal')).toHaveTextContent('Sucesso');
-        expect(getByTestId('modal')).toHaveTextContent('Tarefa Cadastrada com Sucesso!!');
+
+        const modal = await findByTestId('modal');
+
+        expect(modal).toHaveTextContent('Sucesso');
+        expect(modal).toHaveTextContent('Tarefa Cadastrada com Sucesso!!');
     });
 });
