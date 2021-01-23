81a43a9471f36704f42c20ecaa67b72556177641
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 16:37:46 -0300

    Implementaçao dos teste no compoenente alterar

--- frontend/src/components/AtualizarTarefa/atualizaTarefa.test.js
+++ frontend/src/components/AtualizarTarefa/atualizaTarefa.test.js
@@ -2,38 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AtualizarTarefa from './index';
 import Tarefa from '../../models/tarefa.model';
-import {render, fireEvent} from '@testing-library/react';
+import { render, fireEvent, findByTestId } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios';
 
-
-describe.skip('Teste do componente Atualizar Tarefa', () => {
+describe('Teste do componente Atualizar Tarefa', () => {
 
     const tarefaId = 1;
-    const tarefa = new Tarefa(tarefaId, 'novaTarefa', false);
+    //const tarefa = new Tarefa(tarefaId, 'novaTarefa', false);
 
-    beforeEach(() =>{
-        localStorage['tarefas'] = JSON.stringify([tarefa]);
+    // beforeEach(() =>{
+    //     localStorage['tarefas'] = JSON.stringify([tarefa]);
+    // });
+
+    // it('Deve redenrizar sem erros', () => {
+    //     const div = document.createElement('div');
+    //     ReactDOM.render(<AtualizarTarefa id={tarefaId}/>, div);
+    //     ReactDOM.unmountComponentAtNode(div);
+    // });
+
+    it('Deve exibir o modal quando atualizar a tarefa', async () => {
+        axiosMock.get.mockResolvedValueOnce({ data: { nome: 'estudar' } });
+        const { findByTestId } = render(<AtualizarTarefa id={tarefaId} />);
+        fireEvent.click(await findByTestId('btn-atualizar'));
+        const modal = await findByTestId('modal');
+        expect(modal).toHaveTextContent('Sucesso');
     });
 
-    it('Deve redenrizar sem erros', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<AtualizarTarefa id={tarefaId}/>, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
-
-    it('Deve exibir o modal quando atualizar a tarefa', () =>{
-        const {getByTestId} = render(<AtualizarTarefa id={tarefaId} />);
-        fireEvent.click(getByTestId('btn-atualizar'));
-        expect(getByTestId('modal')).toHaveTextContent('Sucesso');
-    });
-
-    it('Deve atualizar uma tarefa', () =>{
-        const nomeTarefaAtualizada = 'tarefaAtualizada';
-        const {getByTestId} = render(<AtualizarTarefa id={tarefaId}/>);
-        fireEvent.change(getByTestId('txt-tarefa'), {target: {value: nomeTarefaAtualizada}});
-        fireEvent.click(getByTestId('btn-atualizar'));
-        const tarefaDb = JSON.parse(localStorage['tarefas']);
-        expect(tarefaDb[0].nome).toBe(nomeTarefaAtualizada);
-    });
+    // it('Deve atualizar uma tarefa', () =>{
+    //     const nomeTarefaAtualizada = 'tarefaAtualizada';
+    //     const {getByTestId} = render(<AtualizarTarefa id={tarefaId}/>);
+    //     fireEvent.change(getByTestId('txt-tarefa'), {target: {value: nomeTarefaAtualizada}});
+    //     fireEvent.click(getByTestId('btn-atualizar'));
+    //     const tarefaDb = JSON.parse(localStorage['tarefas']);
+    //     expect(tarefaDb[0].nome).toBe(nomeTarefaAtualizada);
+    // });
 });
 
