b557ea55e8dd7524ac31a3120234237b5cceb512
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 16:17:10 -0300

    Implementaçao dos teste no compoenente listar tarefas

--- frontend/src/components/ListaTarefas/listarTarefas.test.js
+++ frontend/src/components/ListaTarefas/listarTarefas.test.js
@@ -4,43 +4,64 @@ import ListarTarefas from './index';
 import Tarefa from '../../models/tarefa.model';
 import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios'; 
 
 
-describe.skip('Teste do componente Listar Tarefa', () => {
+describe('Teste do componente Listar Tarefa', () => {
     const nomePrimeiraTarefa = "01";
     const nomeSegundaTarefa = "02";
     const nomeTerceiraTarafa = "03";
 
-    beforeEach(() =>{
-        localStorage['tarefas'] = JSON.stringify([
+    //- INICIO codigo usado para trabalhar com localstorage
+    // beforeEach(() =>{
+    //     localStorage['tarefas'] = JSON.stringify([
+    //         new Tarefa(1, nomePrimeiraTarefa, false),
+    //         new Tarefa(2, nomeSegundaTarefa, false),
+    //         new Tarefa(3, nomeTerceiraTarafa, false)
+    //     ]);
+    // });
+    //- FIM codigo usado para trabalhar com localstorage
+
+    // afterEach(() =>{
+    //     delete localStorage['tarefas'];
+    // });
+    // Teste foi comentado por não haver mais a necessidade dele.
+    // it('Deve redenrizar sem erros', () => {
+    //     const div = document.createElement('div');
+    //     ReactDOM.render(<ListarTarefas />, div);
+    //     ReactDOM.unmountComponentAtNode(div);
+    // });
+
+    const listaTarefa = {
+        totalItems: 3,
+        tarefas: [
             new Tarefa(1, nomePrimeiraTarefa, false),
-            new Tarefa(2, nomeSegundaTarefa, false),
-            new Tarefa(3, nomeTerceiraTarafa, false)
-        ]);
-    });
+            new Tarefa(1, nomeSegundaTarefa, false),
+            new Tarefa(1, nomeTerceiraTarafa, false)
+        ],
+        pagina: 1
+    }
 
-    afterEach(() =>{
-        delete localStorage['tarefas'];
-    });
-
-    it('Deve redenrizar sem erros', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<ListarTarefas />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
-
-    it('Deve exibir uma tabela com 3 tarefas', () =>{
-        const {getByTestId} = render(<ListarTarefas />);
-        const tabela = getByTestId('tabela');
+    it('Deve exibir uma tabela com 3 tarefas', async () =>{
+        axiosMock.get.mockResolvedValue({data: listaTarefa});
+        const {findByTestId} = render(<ListarTarefas />);
+        const tabela = await findByTestId('tabela');
         expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
         expect(tabela).toHaveTextContent(nomeSegundaTarefa);
         expect(tabela).toHaveTextContent(nomeTerceiraTarafa);
     });
 
-    it('Deve filtrar a tarefa', () =>{
-        const {getByTestId} = render(<ListarTarefas />);
-        fireEvent.change(getByTestId('txt-tarefa'), {target: {value: nomePrimeiraTarefa}});
-        const tabela = getByTestId('tabela');
+    it('Deve filtrar a tarefa', async () =>{
+        axiosMock.get.mockResolvedValue({data: listaTarefa});
+        axiosMock.get.mockResolvedValue({data: {
+            totalItems: 1,
+            tarefas: [new Tarefa(1, nomePrimeiraTarefa, false)],
+            pagina: 1
+        }});
+
+        const {findByTestId} = render(<ListarTarefas />);
+        fireEvent.change(await findByTestId('txt-tarefa'), {target: {value: nomePrimeiraTarefa}});
+        const tabela = await findByTestId('tabela');
         expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
         expect(tabela).not.toHaveTextContent(nomeSegundaTarefa);
         expect(tabela).not.toHaveTextContent(nomeTerceiraTarafa);
