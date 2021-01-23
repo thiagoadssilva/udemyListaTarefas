efe49a5c396db58e64d340603da22bacd5cbab1a
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 16:47:08 -0300

    Implementaçao dos teste no compoenente Remover

--- frontend/src/components/RemoverTarefa/removerTarefa.test.js
+++ frontend/src/components/RemoverTarefa/removerTarefa.test.js
@@ -4,17 +4,18 @@ import RemoverTarefa from './index';
 import Tarefa from '../../models/tarefa.model';
 import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios';
 
-describe.skip("Teste do componente remover tarefa", () =>{
+describe("Teste do componente remover tarefa", () =>{
     const nomeTarefa = "lla";
     const tarefa = new Tarefa(1, nomeTarefa, false);
 
-    it("Deve renderizar o componente sem erros", () =>{
-        const div = document.createElement('div');
-        ReactDOM.render(
-            <RemoverTarefa tarf={tarefa} recarregarTarefas={() => false}/>, div);
-        ReactDOM.unmountComponentAtNode(div);
-    }); 
+    // it("Deve renderizar o componente sem erros", () =>{
+    //     const div = document.createElement('div');
+    //     ReactDOM.render(
+    //         <RemoverTarefa tarf={tarefa} recarregarTarefas={() => false}/>, div);
+    //     ReactDOM.unmountComponentAtNode(div);
+    // }); 
 
     it('Deve Exibir o modal sem erros', () =>{
         const {getByTestId} = render(
@@ -23,13 +24,15 @@ describe.skip("Teste do componente remover tarefa", () =>{
         expect(getByTestId('modal')).toHaveTextContent(nomeTarefa);
     });
 
-    it('Deve remover uma tarefa', () =>{
-        localStorage['tarefas'] = JSON.stringify([tarefa]);
-        const {getByTestId} = render(
+    it('Deve remover uma tarefa', async () =>{
+        //localStorage['tarefas'] = JSON.stringify([tarefa]);
+        const {getByTestId, findByTestId} = render(
             <RemoverTarefa tarf={tarefa} recarregarTarefas={() => false}/>);
         fireEvent.click(getByTestId('btn-abrir-modal'));
         fireEvent.click(getByTestId('btn-remover'));
-        const tarefasDb = JSON.parse(localStorage['tarefas']);
-        expect(tarefasDb.length).toBe(0);
+        //const tarefasDb = JSON.parse(localStorage['tarefas']);
+        //expect(tarefasDb.length).toBe(0);
+        await findByTestId('modal');
+        expect(axiosMock.delete).toHaveBeenCalledTimes(1);
     });
 });
